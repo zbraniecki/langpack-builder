10aaaafd1e6072b23b79381cf6809658746b92e7
Zibi Braniecki <gandalf@mozilla.com>
2014-11-12 14:57:06 -0800

    I think I changed some thing

--- bin/lp-builder.js
+++ bin/lp-builder.js
@@ -2,120 +2,67 @@
 
 'use strict';
 
-
 var program = require('commander');
 var fs = require('fs');
 var path = require('path');
-var wrench = require('wrench');
-var Promise = require('promise');
-var Set = require('es6-set');
 
 var utils = require('../lib/utils');
+var lpBuilder = require('../lib/lp-builder');
 
-function buildLangpack(gaiaPath, localePath, resultPath) {
+function buildLangpack(gaiaPath, localePath, resultPath, locale) {
   utils.cleanDir(resultPath);
 
-  fs.mkdirSync(path.join(resultPath, 'apps'));
-  var apps = fs.readdirSync(path.join(gaiaPath, 'apps'));
+  fs.mkdirSync(path.join(resultPath, locale));
+  fs.mkdirSync(path.join(resultPath, locale, 'apps'));
+  var apps = utils.getDirs(path.join(gaiaPath, 'apps'));
   //var apps = ['settings'];
 
+  addLangpackManifest(resultPath, locale, apps);
+
   apps.forEach(function(app) {
-    var appPath = path.join(gaiaPath, 'apps', app);
-    if (utils.isDirectory(appPath)) {
-      getResourcesFromHTMLFiles(gaiaPath, path.join(gaiaPath, 'apps', app))
-        .then(function(resList) {
-          copyAppData(localePath, resultPath, app, resList);
-        }
-      );
-    }
+    var appPath = path.join(gaiaPath, locale, 'apps', app);
+    lpBuilder.getResourcesFromHTMLFiles(gaiaPath,
+      path.join(gaiaPath, 'apps', app))
+      .then(function(resList) {
+        lpBuilder.copyAppData(localePath, path.join(resultPath, locale), app, resList);
+      }
+    );
   });
 }
 
 // Lib functions
 
-function copyAppData(localePath, resultPath, app, resList) {
-  var sourceAppPath = path.join(localePath, 'apps', app);
-  var resultAppPath = path.join(resultPath, 'apps', app, 'locales'); 
+function addLangpackManifest(resultPath, locale, apps) {
+  var manifest = {};
 
-  resList.forEach(function(resPath) {
-    var pathChunks = utils.splitPath(resPath);
+  manifest.target = {
+    app: 'Gaia',
+    version: '2.2'
+  };
+  manifest.version = '1';
+  manifest.role = 'langpack';
+  manifest['languages-provided'] = {};
 
-    if (pathChunks[0] === 'shared') {
-      pathChunks = pathChunks.filter(function(chunk) {
-        return chunk !== 'locales';
-      });
-      var newPath = path.join.apply(path, pathChunks).replace('.{locale}.', '.');
-      var inPath = path.join(localePath, newPath); 
-      var outPath = path.join(resultPath, 'apps', app, newPath);
-
-      if (!fs.existsSync(inPath)) {
-        console.warn('Warning! Missing file: ' + inPath);
-        return;
-      }
-      utils.copyFile(inPath, outPath);
-    } else {
-      pathChunks = pathChunks.filter(function(chunk) {
-        return chunk !== 'locales';
-      });
-      var newPath = path.relative(path.join('apps', app),
-        path.join.apply(path, pathChunks)).replace('.{locale}.', '.');
-      var inPath = path.join(sourceAppPath, newPath); 
-      var outPath = path.join(resultPath, 'apps', app, 'locales', newPath);
-      
-      if (!fs.existsSync(inPath)) {
-        console.warn('Warning! Missing file: ' + inPath);
-        return;
-      }
-      utils.copyFile(inPath, outPath);
-    }
+  apps.forEach(function(app) {
+    var appID = 'app://' + app + '.gaiamobile.org/manifest.webapp';
+    var langs = {};
+    langs[locale] = '/' + locale + '/apps/' + app;
+    manifest['languages-provided'][appID] = langs;
   });
 
-  var manifestPath = path.join(sourceAppPath, 'manifest.properties');
-
-  if (fs.existsSync(manifestPath)) {
-    utils.copyFile(manifestPath,
-      path.join(resultPath, 'apps', app, 'manifest.properties'));
-  }
-}
-
-function flattenResources(gaiaPath, resLists) {
-  var flatResources = new Set();
-  resLists.forEach(function(resList) {
-    resList.forEach(function(resPath) {
-      var fullResPath = utils.buildResourcePath(resPath, gaiaPath);
-      if (Array.isArray(fullResPath)) {
-        fullResPath.forEach(function(p) {
-          flatResources.add(p);
-        });
-      } else {
-        flatResources.add(fullResPath);
-      }
-    });
-  });
-
-  return flatResources;
-}
-
-function isNotTestFile(path) {
-  return path.indexOf('/test/') === -1;
-}
-
-function getResourcesFromHTMLFiles(gaiaPath, appPath) {
-    var htmlPaths = utils.ls(appPath, true, /\.html$/).filter(isNotTestFile);
-    return Promise.all(
-      htmlPaths.map(
-        utils.getResourcesFromHTMLFile.bind(null, gaiaPath)))
-          .then(flattenResources.bind(null, gaiaPath));
+  utils.writeFile(path.join(resultPath, 'manifest.webapp'), JSON.stringify(manifest, false, 2));
 }
 
 program
   .version('0.0.1')
   .usage('[options] locale-path result-path')
   .option('-g, --gaia <dir>', 'Gaia dir')
+  .option('-l, --locale <locale>', 'Locale')
   .parse(process.argv);
 
 var localePath = program.args[0];
 var resultPath = program.args[1];
 var gaiaPath = program.gaia;
+var locale = program.locale;
 
-buildLangpack(gaiaPath, localePath, resultPath);
+buildLangpack(gaiaPath, localePath, resultPath, locale);
