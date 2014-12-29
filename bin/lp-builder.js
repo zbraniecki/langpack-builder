4cda47188d40b326d830534cf3755abb930da0b5
Zibi Braniecki <gandalf@mozilla.com>
2014-12-28 22:15:25 -0800

    Update manifest.webapp

--- bin/lp-builder.js
+++ bin/lp-builder.js
@@ -9,7 +9,8 @@ var path = require('path');
 var utils = require('../lib/utils');
 var lpBuilder = require('../lib/lp-builder');
 
-function buildLangpack(gaiaPath, localePath, resultPath, locale) {
+function buildLangpack(gaiaPath, localePath, resultPath,
+    locale, releaseUrl, name) {
   utils.cleanDir(resultPath);
 
   fs.mkdirSync(path.join(resultPath, locale));
@@ -17,7 +18,7 @@ function buildLangpack(gaiaPath, localePath, resultPath, locale) {
   var apps = utils.getDirs(path.join(gaiaPath, 'apps'));
   //var apps = ['settings'];
 
-  addLangpackManifest(resultPath, [locale], apps);
+  addLangpackManifest(resultPath, [locale], apps, releaseUrl, name);
 
   apps.forEach(function(app) {
     var appPath = path.join(gaiaPath, locale, 'apps', app);
@@ -50,11 +51,16 @@ function getTimestamp(date) {
   }).join('');
 }
 
-function addLangpackManifest(resultPath, locales, apps) {
+function addLangpackManifest(resultPath, locales, apps, releaseUrl, name) {
   var manifest = {};
 
+  manifest.name = name;
   manifest.role = 'langpack';
   manifest.version = '1.0.0';
+  manifest.developer = {
+    'name': 'Mozilla',
+    'url': releaseUrl
+  };
   manifest['languages-target'] = {
     'app://*.gaiamobile.org/manifest.webapp': '2.2'
   };
@@ -67,8 +73,8 @@ function addLangpackManifest(resultPath, locales, apps) {
       origins[appID] = '/' + locale + '/apps/' + app;
     });
     manifest['languages-provided'][locale] = {
-      version: getTimestamp(),
-      origins: origins
+      version: parseInt(getTimestamp()),
+      apps: origins
     };
   });
   utils.writeFile(path.join(resultPath, 'manifest.webapp'), JSON.stringify(manifest, false, 2));
@@ -79,15 +85,21 @@ program
   .usage('[options] locale-path')
   .option('-g, --gaia <dir>', 'Gaia dir')
   .option('-l, --locale <locale>', 'Locale')
+  .option('-r, --release-url <url>', 'URL used to distribute the package')
+  .option('-j, --json', 'pack json files')
+  .option('-s, --source', 'pack source files')
+  .option('-n, --name <string>', 'langpack name')
   .parse(process.argv);
 
 var localePath = program.args[0];
 var resultPath = './out/';
 var gaiaPath = program.gaia;
 var locale = program.locale;
+var releaseUrl = program.releaseUrl;
+var name = program.name;
 
 if (!locale || !gaiaPath || program.args.length !== 1) {
   console.log('Example: ./bin/lp-builder.js --gaia /path/to/gaia --locale ab-CD /path/to/gaia-l10n/ab-CD');
   return;
 }
-buildLangpack(gaiaPath, localePath, resultPath, locale);
+buildLangpack(gaiaPath, localePath, resultPath, locale, releaseUrl, name);
